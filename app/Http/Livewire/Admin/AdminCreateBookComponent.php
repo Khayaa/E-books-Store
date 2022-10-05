4529eea52bca8c174382ade2643f538f5bea7f03
Khaya Ngwenya <khaya.developer@gmail.com>
2022-10-05 20:15:32 +0200

    Create Book Component

---
+++ app/Http/Livewire/Admin/AdminCreateBookComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class AdminCreateBookComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.admin-create-book-component');
+    }
+}
---
+++ app/Http/Livewire/Seller/SellerCreateBookComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Seller;
+
+use Livewire\Component;
+
+class SellerCreateBookComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.seller.seller-create-book-component');
+    }
+}
